3330f0ef424ab07db89294d8479675c571fd02ff
Perla Ordonez <guate.po@gmail.com>
2019-03-12 16:27:22 -0500

    updates

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,30 +4,38 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 router.get("/", function(req, res) {
-  res.redirect("/burgers");
-});
-
-router.get("/burgers", function(req, res) {
-  burger.all(function(burgerData) {
-    var hbsObject={
-      burger: burgerData
+  burger.all(function(data) {
+    var hbsObject = {
+      burger: data
     };
+    console.log("hbsObject", hbsObject);
     res.render("index", hbsObject);
   });
 });
 
-router.post("/burgers/create", function(req, res) {
-  burger.create(req.body.burger_name, function(result) {
-    console.log(result);
-    res.redirect("/");
+router.post("/api/burgers", function(req, res) {
+  burger.create([
+    "burger_name", "devoured"
+  ], [
+    req.body.burger_name, req.body.devoured
+  ], function(result) {
+    res.json({ id: result.insertId });
   });
 });
 
-router.put("/burgers/:id", function(req, res) {
-  burger.update(req.params.id, function(result) {
-    console.log(result);
-    res.sendStatus(200);
+router.put("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.update({
+    devoured: req.params.devoured
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
   });
 });
-
 module.exports = router;
